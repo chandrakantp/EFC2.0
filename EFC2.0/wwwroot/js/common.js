3c064d8341c5ca3bbcccbf65cf435494266b7104
patilchandrakant98 <patilchandrakant98@outlook.com>
2025-04-01 15:00:02 +0530

    css changes pushed

---
+++ EFC2.0/wwwroot/js/common.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function () {
+    $("#otpbtn").click(function () {
+        $("#login-form-withOTP").show(); 
+        $("#login-form").hide(); 
+    });
+
+    $("#backToLogin").click(function () {
+        $("#login-form-withOTP").hide();
+        $("#login-form").show();
+    });
+
+});
