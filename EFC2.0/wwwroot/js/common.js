1910d2e2fa65d66af374652caf60aefb4a777b87
patilchandrakant98 <patilchandrakant98@outlook.com>
2025-04-01 17:41:18 +0530

    login page done

--- EFC2.0/wwwroot/js/common.js
+++ EFC2.0/wwwroot/js/common.js
@@ -1,13 +1,37 @@
-﻿
+﻿// Define functions globally
+function otpbtn() {
+    $("#login-form-withOTP").show();
+    $("#login-form").hide();
+    $("#forgot-pass-form").hide();
+}
+
+function backToLogin() {
+    $("#login-form-withOTP").hide();
+    $("#login-form").show();
+    $("#forgot-pass-form").hide();
+}
+
+function forgotPassword() {
+    $("#login-form-withOTP").hide();
+    $("#login-form").hide();
+    $("#forgot-pass-form").show();
+}
+
 $(document).ready(function () {
-    $("#otpbtn").click(function () {
-        $("#login-form-withOTP").show(); 
-        $("#login-form").hide(); 
+    $("#login-form-withOTP").hide();
+    $("#forgot-pass-form").hide();
+
+    $("#invisiblepass").hide(); // Hide the 'visibility_on' icon initially
+
+    $("#visiblepass").click(function () {
+        $("#visiblepass").hide();
+        $("#invisiblepass").show();
+        $("#mat-input-3").attr("type", "text"); // Change password field to text
     });
 
-    $("#backToLogin").click(function () {
-        $("#login-form-withOTP").hide();
-        $("#login-form").show();
+    $("#invisiblepass").click(function () {
+        $("#visiblepass").show();
+        $("#invisiblepass").hide();
+        $("#mat-input-3").attr("type", "password"); // Change text field back to password
     });
-
 });
