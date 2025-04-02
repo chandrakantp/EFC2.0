adbd4897a4511aec4cca683d2d999e2afafa6fb6
parasharam <parasharam.sawant@databridgemarketresearch.in>
2025-04-02 19:22:06 +0530

    UI changes

---
+++ EFC2.0/Pages/Shared/header.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFC2._0.Pages.Shared
+{
+    public class headerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
