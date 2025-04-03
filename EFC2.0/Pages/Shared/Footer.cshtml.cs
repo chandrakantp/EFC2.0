eacdf2c164a1e234177aeea3f7006d1abb071511
parasharam <parasharam.sawant@databridgemarketresearch.in>
2025-04-03 15:46:45 +0530

    fyuhiu

---
+++ EFC2.0/Pages/Shared/Footer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFC2._0.Pages.Shared
+{
+    public class FooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
