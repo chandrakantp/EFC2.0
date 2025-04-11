dded81d67fd926e7be6a6bc64372baf46c39c367
parasharam <parasharam.sawant@databridgemarketresearch.in>
2025-04-11 17:37:38 +0530

    done

---
+++ EFC2.0/Pages/Organization/Org_Employees/In_Probation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFC2._0.Pages.Organization.Org_Employees
+{
+    public class In_ProbationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
